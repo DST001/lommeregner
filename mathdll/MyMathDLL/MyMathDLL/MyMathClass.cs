abba43d515a217df22b0fdd438370faf2b026ddc
DST001 <davidt72@yahoo.dk>
2018-09-17 12:28:55 +0200

    a

--- mathdll/MyMathDLL/MyMathDLL/MyMathClass.cs
+++ mathdll/MyMathDLL/MyMathDLL/MyMathClass.cs
@@ -8,6 +8,12 @@ namespace MyMathDLL
 {
     public class MyMathClass
     {
+        /// <summary>
+        /// Lommeregner
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
         public int Add(int a, int b)
         {
             return a + b;
